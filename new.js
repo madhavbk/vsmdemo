a6169034df2663520049f1e2f02f377997dad40f
ponnaruvi100 <ponnaruvi100@gmail.com>
2026-08-26 18:22:06 +0530

    Create new.js

---
+++ new.js
@@ -0,0 +1,44 @@
+const name = "John";
+const age = 25;
+
+console.log(name);
+console.log(age);
+
+// Error 1: Missing value
+const city = ;
+
+// Error 2: Undefined variable
+console.log(country);
+
+// Error 3: Missing closing parenthesis
+console.log("Hello";
+
+// Error 4: Incorrect if syntax
+if (age > 18 {
+    console.log("Adult");
+}
+
+// Error 5: Assignment instead of comparison
+if (age = 25) {
+    console.log("Age is 25");
+}
+
+// Error 6: Invalid function syntax
+function greet( {
+    console.log("Hello");
+}
+
+// Error 7: Missing closing bracket
+const numbers = [1, 2, 3;
+
+// Error 8: Invalid object syntax
+const person = {
+    name: "John"
+    age: 25
+};
+
+// Error 9: Calling undefined function
+sayHello();
+
+// Error 10: Invalid JSON
+const data = JSON.parse('{name: "John"}');
