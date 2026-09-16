d2a0bf7873dd9854318524dfadb1b4237be14bfb
ponnaruvi100 <ponnaruvi100@gmail.com>
2026-09-17 00:23:56 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b
+        System.out.println("Sum: " + sum);
+
+        int product = a * c;
+        System.out.println("Product: " + product);
+
+        String name = "John;
+        System.out.println("Hello " + name);
+
+        if (a < b) {
+            System.out.println("a is smaller than b")
+        }
+
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(numbers[5]);
+    }
+}
