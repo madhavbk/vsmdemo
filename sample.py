0345ad4b8c33c2cbb24274271c479521a2e8247f
ponnaruvi100 <ponnaruvi100@gmail.com>
2026-09-17 00:10:28 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,15 @@
+def calculate_average(numbers):
+    total = sum(numbers)
+    average = total / len(numbers
+    return average
+
+numbers = [10, 20, 30, 40, 50]
+
+print("Numbers:", numbers)
+print("Average:", calculate_average(number))
+
+for i in range(len(numbers)):
+    print("Number", i + 1, "=", numbers[i]
+
+if average > 25:
+    print("The average is high")
